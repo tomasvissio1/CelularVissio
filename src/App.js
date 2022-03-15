eaeaca2e387d6c17cba9ee7ee1054e82644046c1
tomasvissio1 <pugsinccba@gmail.com>
2022-03-14 21:39:01 -0300

    app e commerce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="App" onClick={()=>console.log('soy evento')}>
+      <NavBar />
+      <header className="App-header" style={{'backgroundColor':"white"}}>
+        
+        
+        <img src={logo} className="App-logo" alt="logo"  />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
